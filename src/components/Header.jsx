c135693a4db2d41e81a83aafbab613dde7626161
fredy <fredy-andri.mottus@voco.ee>
2025-03-21 11:47:34 +0200

    ül4 startipauk, failid olemas ja loodud, aeg teha sisu

---
+++ src/components/Header.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+    return (
+        <header>
+            <h1>Demo Website</h1>
+            <button>Toggle Theme</button>
+        </header>
+    )
+}
+
+export default Header
---
+++ src/components/Page.jsx
@@ -0,0 +1,18 @@
+import Header from './Header.jsx'
+
+const Page = () => {
+    return (
+        <div id='app'>
+            <Header />
+
+            <article>
+                <h2>React Course</h2>
+                <p>
+                    A course that teaches you React.
+                </p>
+            </article>
+        </div>
+    )
+}
+
+export default Page
